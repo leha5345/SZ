15e9de372b17b04bbae2ceabefcfb43902e2e3af
JIexaBek <bek@uix.ru>
2023-01-21 20:53:31 +0300

    справка по скриптам

---
+++ Help.sql
@@ -0,0 +1,39 @@
+﻿---Cоздание моментального снимка базы DAX2009_RP
+/*
+CREATE DATABASE DAX2009_RP_001 ON
+( NAME = DAX2009_RP , FILENAME = 'C:\SQL\0\DAX2009_RP_001.ss' ) -- Указывать на SQL сервере займет 457Гб свободного места (правда непонятно как создается так как при создании было всего 80Гб)
+
+AS SNAPSHOT OF DAX2009_RP;
+GO
+--Удаление снимка БД 
+USE master
+GO
+IF DB_ID (N'DAX2009_RP_001') IS NOT NULL
+	DROP DATABASE DAX2009_RP_001;
+GO
+
+
+
+-- Создание базы
+CREATE DATABASE W_test;
+GO
+
+--Удаление базы
+USE master
+GO
+IF DB_ID (N'W_test_001') IS NOT NULL
+	DROP DATABASE W_test_001;
+GO
+
+EXEC sp_help CUSTTABLE
+
+
+
+---9 Создание синонима
+use [W_test]
+GO
+CREATE SYNONYM TenderTable FOR DAX2009_RP.dbo.TenderTable;
+
+use [W_test];
+select * from TenderTable;
+*/
