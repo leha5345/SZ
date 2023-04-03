a185b0dccc384964b6f884b63b070190a5fff1b3
JIexaBek <bek@uix.ru>
2023-04-03 16:06:08 +0300

    табличка для отгрузочных

---
+++ FixingDate.sql
@@ -0,0 +1,16 @@
+﻿
+USE [DAX2009_1]
+GO
+SELECT WMSPickingRoute.transRefId
+	,FixingDateTimeSalesTtransfers.MINDATESHIPMENT -- Дата накладной + 3
+	,DATEADD(hour, +3,WMSPickingRoute.DlvDate) AS  DlvDate
+	,DATEADD(hour, +3,WMSPickingRoute.DlvDateMP) AS  DlvDateMP
+	--,WMSPickingRoute.DlvDate
+	--,WMSPickingRoute.DlvDateMP
+	,'///////////'
+	,FixingDateTimeSalesTtransfers.CURRENTDATETIME -- Дата создания + 3
+	,DATEADD(hour, +3,WMSPickingRoute.CREATEDDATETIME) AS  CREATEDDATETIME
+--	,WMSPickingRoute.CREATEDDATETIME
+FROM WMSPickingRoute
+LEFT JOIN FIXINGDATETIMESALESTTRANSFERS ON WMSPickingRoute.transRefId = FIXINGDATETIMESALESTTRANSFERS.SALESID
+WHERE WMSPickingRoute.transRefId = '00603706_069' 
